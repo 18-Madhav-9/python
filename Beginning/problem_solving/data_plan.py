dd3060c874e2be630b1994f401bba842faad89d9
madhav_18 <159808252+18-Madhav-9@users.noreply.github.com>
2024-07-14 18:10:09 +0530

    Add files via upload

---
+++ Beginning/problem_solving/data_plan.py
@@ -0,0 +1,16 @@
+"""
+number of megabytes of data that user uses in each of
+the first n months. Our task is to determine the number of megabytes avail-
+able for the following month
+"""
+
+x = int(input("Enter Number of monthly Data Given :"))
+n = int(input("Enter number of months in data used :"))
+
+data_avail = 0 
+
+for i in range(n):
+    data_used = input("Enter data each month used :")
+    data_avail = x + data_avail - data_used
+
+print("data available for next month ", data_avail + x )
